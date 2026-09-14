0f15a9db6ee5d6e3f6d42f581541a4d5eb2c0dea
Ayoub Tajana <ayoubtajana@proximus.be>
2026-09-14 20:36:05 +0200

    added 0.5 seconds delay using BIOS interrupt functions to both texts

--- boot/version1.1/bootloader.s
+++ boot/version1.1/bootloader.s
@@ -15,27 +15,45 @@ _start:
 
 
 printwelcome:
-    # print A
-    mov $0x0E, %ah
-    mov (%si), %al
-    int $0x10
+    # 2 second delay
+        mov $0x86, %ah
+        mov $0x00, %al
+        mov $0x0007, %cx
+        mov $0xA120, %dx
+        int $0x15
 
-    inc %si
-    loop printwelcome
+    # print text to screen
+        mov $0x0E, %ah
+        mov (%si), %al
+        int $0x10
+
+        inc %si
+        loop printwelcome
     
-    mov $loadingmsg, %si
-    mov $17,%cx
-    jmp printloading_kernel
+        mov $loadingmsg, %si
+        mov $17,%cx
+        jmp printloading_kernel
 
 printloading_kernel:
-    mov $0x0E,%ah
-    mov (%si), %al
-    int $0x10
+    
+    # 2 second delay
+        mov $0x86, %ah
+        mov $0x00, %al
+        mov $0x0007, %cx
+        mov $0xA120, %dx
+        int $0x15
+    
+    # enabling output to the screen (BIOS Function)
+        mov $0x0E,%ah
 
-    inc %si
-    loop printloading_kernel
+    # Copy the character at the adress stored in REG %SI
+        mov (%si), %al
+        int $0x10 # BIOS Interrupt
 
-    jmp .
+        inc %si
+        loop printloading_kernel
+
+        jmp .
 welcomemessage:
     .ascii "The Mountain Systems Bootloader v1.0 \n\r"
 loadingmsg:
