c2681af6119306f82a7f4da8e98512f8101c5f06
Ayoub Tajana <ayoubtajana@proximus.be>
2026-09-14 22:32:47 +0200

    update Makefile and creation of version2.0

--- Makefile
+++ Makefile
@@ -7,3 +7,8 @@ bootv1.1:
 	as --32 ./boot/version1.1/bootloader.s -o ./boot/version1.1/object_files/bootloader.o
 	ld -m elf_i386 -Ttext 0x7C00 --oformat binary ./boot/version1.1/object_files/bootloader.o -o ./boot/version1.1/executables/bootloader.bin
 	qemu-system-x86_64 -drive format=raw,file=./boot/version1.1/executables/bootloader.bin
+
+bootv2:
+	as --32 ./boot/version2.0/bootloader.s -o ./boot/version2.0/object_files/bootloader.o
+	ld -m elf_i386 -Ttext 0x7C00 --oformat binary ./boot/version2.0/object_files/bootloader.o -o ./boot/version2.0/executables/bootloader.bin
+	qemu-system-x86_64 -drive format=raw,file=./boot/version2.0/executables/bootloader.bin
---
+++ boot/version2.0/bootloader.s
@@ -0,0 +1,64 @@
+# This program has been made by The Mountain Systems 2026 Copyright
+.global _start
+.section .text
+.code16
+
+_start:
+    #Set video mode
+    mov $0x0, %ah
+    mov $0x3, %al
+    int $0x10
+    
+    
+    mov $welcomemessage, %si
+    mov $39,%cx
+
+
+printwelcome:
+    push %cx
+    # 2 second delay
+        mov $0x86, %ah
+        mov $0x00, %al
+        mov $0x0001, %cx
+        mov $0x86A0, %dx
+        int $0x15
+
+    pop %cx
+    # print text to screen
+        mov $0x0E, %ah
+        mov (%si), %al
+        int $0x10
+
+        inc %si
+        loop printwelcome
+    
+        mov $loadingmsg, %si
+        mov $17,%cx
+        jmp printloading_kernel
+
+printloading_kernel:
+     push %cx
+    # 2 second delay
+        mov $0x86, %ah
+        mov $0x00, %al
+        mov $0x0001, %cx
+        mov $0x86A0, %dx
+        int $0x15
+     pop %cx
+    # enabling output to the screen (BIOS Function)
+        mov $0x0E,%ah
+
+    # Copy the character at the adress stored in REG %SI
+        mov (%si), %al
+        int $0x10 # BIOS Interrupt
+
+        inc %si
+        loop printloading_kernel
+
+        jmp .
+welcomemessage:
+    .ascii "The Mountain Systems Bootloader v1.0 \n\r"
+loadingmsg:
+    .ascii "Loading Kernel..."
+    .fill 510 - (. - _start), 1, 0
+    .word 0xAA55
